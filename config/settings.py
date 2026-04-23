8d81c952db46bf513df352666d60c627fd9e7a84
GetApple <getapple@MacBook-Air-KSS.local>
2026-04-23 13:14:43 +0300

    refactoring

--- config/settings.py
+++ config/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 
