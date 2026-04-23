69ee52a8955b27f8f82ba35e6f1e7f2caab89ece
GetApple <getapple@MacBook-Air-KSS.local>
2026-04-23 21:38:43 +0300

    refactoring 15

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
-from django.conf.urls.static import static
-
+from django.views.static import serve
 from seo.views import robots
 
 
@@ -20,8 +19,9 @@ urlpatterns = [
 ]
 
 
-# MEDIA только через static()
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+# 🔥 КОСТЫЛЬ ДЛЯ MEDIA (в самом конце!)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {
+        "document_root": settings.MEDIA_ROOT,
+    }),
+]
