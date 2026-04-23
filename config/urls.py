f70f3ffacc174e510622c8ba78b714a85c694a57
GetApple <getapple@MacBook-Air-KSS.local>
2026-04-23 13:17:19 +0300

    refactoring 1

--- config/urls.py
+++ config/urls.py
@@ -1,27 +1,27 @@
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.conf import settings
-from django.views.static import serve
+from django.conf.urls.static import static
 
 from seo.views import robots
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("", include(("main.urls", "main"), namespace="main")),
     path("", include(("services.urls", "services"), namespace="services")),
     path("", include(("portfolio.urls", "portfolio"), namespace="portfolio")),
-    path("cookies/", include("cookie_consent.urls")),
-    path("robots.txt", robots)
-]
 
-urlpatterns += [
+    path("cookies/", include("cookie_consent.urls")),
+    path("robots.txt", robots),
+
     path("tinymce/", include("tinymce.urls")),
 ]
 
-# 🔥 КОСТЫЛЬ ДЛЯ MEDIA (в самом конце!)
-urlpatterns += [
-    re_path(r"^media/(?P<path>.*)$", serve, {
-        "document_root": settings.MEDIA_ROOT,
-    }),
-]
+
+# MEDIA только через static()
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
